8dab9f3dd3c6a644a92e31d9c95e78e7b0ec9afc
Billie Simmons <billiesnyc@gmail.com>
2018-08-30 15:31:02 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -4,11 +4,13 @@ class Person
   
   @@bank_account = 0
   @@happiness_points = 0
+  @@hygiene_points = 0
   
   def initialize(name)
     @name = name
     @@bank_account += 25
     @@happiness_points += 8
+    @@hygiene_points += 8
   end
   
 end
