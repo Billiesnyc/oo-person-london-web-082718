3e53a4826be62fc9a11b8e7ed1616d135133200c
Billie Simmons <billiesnyc@gmail.com>
2018-08-30 15:44:58 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,15 +2,15 @@ class Person
   attr_accessor :bank_account, :happiness_points, :hygiene_points
   attr_reader :name
   
-  @bank_account = 0
-  @happiness_points = 0
-  @hygiene_points = 0
+  @@bank_account = 0
+  @@happiness_points = 0
+  @@hygiene_points = 0
   
   def initialize(name)
     @name = name
-    @bank_account += 25
-    @happiness_points += 8
-    @hygiene_points += 8
+    @@bank_account += 25
+    @@happiness_points += 8
+    @@hygiene_points += 8
   end
   
   def clean?
