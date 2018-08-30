8c8f14a339673492400e516a349ec00860953974
Billie Simmons <billiesnyc@gmail.com>
2018-08-30 15:50:22 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,7 +2,7 @@ class Person
   attr_accessor :bank_account, :happiness_points, :hygiene_points
   attr_reader :name
   
-  @@bank_account = 25
+  @bank_account = 25
   @@happiness_points = 8
   @@hygiene_points = 8
   
