c1215c590c096e0e8a897667bac365b497146825
Billie Simmons <billiesnyc@gmail.com>
2018-08-30 15:30:46 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,10 +3,12 @@ class Person
   attr_reader :name
   
   @@bank_account = 0
+  @@happiness_points = 0
   
   def initialize(name)
     @name = name
     @@bank_account += 25
+    @@happiness_points += 8
   end
   
 end
